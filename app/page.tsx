14fb346c79a735d2b263af662c00884ea5d557ee
Juan Prado <juanprado@MacBook-Pro.local>
2023-05-21 22:33:23 -0400

    Setting up the thing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    Hello World. <Link href="/about">About</Link>
+  </div>
+)
+
+export default Home;
--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 // @ts-check
 
+const isProd = process.env.NODE_ENV === 'production'
+
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   output: 'export',
-  basePath: '/espooky',
+  basePath: isProd ? '/espooky' : '',
   experimental: {
     appDir: true,
+  },
+  images: {
+    unoptimized: true,
   },
 }
 
