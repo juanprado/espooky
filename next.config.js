bbb8a483abd3a2913d6dd74ef8959dab366ca117
Juan Prado <juanprado@MacBook-Pro.local>
2023-05-21 23:08:26 -0400

    maybe this will fix it??

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const isProd = process.env.NODE_ENV === 'production'
  **/
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/espooky' : '',
+  assetPrefix: isProd ? '/espooky/' : '',
   experimental: {
     appDir: true,
   },
