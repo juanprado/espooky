7fbba76a1bb1d889bdc671e7e8a6492a8e23b4f2
Juan Prado <juanprado@MacBook-Pro.local>
2023-05-29 15:13:43 +0200

    no longer using github pages

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-// @ts-check
-
-const isProd = process.env.NODE_ENV === 'production'
-
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  output: 'export',
-  assetPrefix: isProd ? `/espooky` : "",
-  basePath: isProd ? `/espooky` : "",
   experimental: {
     appDir: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
